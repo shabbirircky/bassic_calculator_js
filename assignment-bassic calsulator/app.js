28f4253085cfba89a5a181d4988ae122d4d401a6
shabbirricky. adnanhossain157@gmail.com <adnanhossain157@gmail.com>
2023-08-11 17:54:29 +0600

    2nd commit

---
+++ assignment-bassic calsulator/app.js
@@ -0,0 +1,35 @@
+
+const Plus =(FastNumber,SecoundNumber)=>{
+    let result= FastNumber + SecoundNumber ;
+    console.log(result);
+    return result;
+    
+}
+
+const Minus =(FastNumber,SecoundNumber)=>{
+    let result= FastNumber - SecoundNumber ;
+    console.log(result);
+    return result;
+    
+}
+
+const Divide =(FastNumber,SecoundNumber)=>{
+    let result= FastNumber / SecoundNumber ;
+    console.log(result);
+    return result;
+    
+}
+
+const Multiply =(FastNumber,SecoundNumber)=>{
+    let result= FastNumber * SecoundNumber ;
+    console.log(result);
+    return result;
+    
+}
+
+// ---------------------------------- end const--------------------
+
+Plus(50,70);
+Minus(90,70 )
+Divide(90,6 )
+Multiply(9,6 )
